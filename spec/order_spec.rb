9d6fd2ae706e6856d0d95dc5454d1fb66bb3320d
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2014-11-27 19:58:08 +0000

    adding to order test added

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -2,7 +2,8 @@ require 'order'
 
 describe Order do
   
-  let (:new_order) { Order.new }
+  let (:new_order)  { Order.new }
+  let (:dish)       { double :dish, :name => "Naan", :price => 3 }
 
   it 'does not have any items when created' do
     expect(new_order.items).to be_empty
@@ -10,6 +11,11 @@ describe Order do
   
   it 'should have a total of zero when created' do
     expect(new_order.order_total).to be 0
+  end
+
+  it 'can have dishes added to it by a customer' do
+    new_order.add(dish)
+    expect(new_order.contains?(dish.name)).to be true
   end
 
 end
